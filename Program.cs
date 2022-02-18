2ce2be5d38ba41f348d3f1ba7544b596c6b2258a
Rodrigo Caballero <RCaball3@its.jnj.com>
2022-02-18 19:14:02 -0400

    init code

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Azure.KeyVault;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+Console.WriteLine("Retrieving secret...");
+
+var baseUri = Environment.GetEnvironmentVariable("MFLOW_TEST_BASE_URL");
+if(string.IsNullOrEmpty(baseUri))
+    Console.WriteLine($"Env Var not found: MFLOW_TEST_BASE_URL");
+
+var clientId = Environment.GetEnvironmentVariable("MFLOW_TEST_CLIENT_ID");
+if(string.IsNullOrEmpty(clientId))
+    Console.WriteLine($"Env Var not found: MFLOW_TEST_CLIENT_ID");
+
+var clientSecret = Environment.GetEnvironmentVariable("MFLOW_TEST_CLIENT_SECRET");
+if(string.IsNullOrEmpty(clientSecret))
+    Console.WriteLine($"Env Var not found: MFLOW_TEST_CLIENT_SECRET");
+
+var client = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(
+    async (string auth, string res, string scope) => 
+    {
+        var authContext = new AuthenticationContext(auth);
+        var credential = new ClientCredential(clientId, clientSecret);
+        var result = await authContext.AcquireTokenAsync(res, credential);
+        if (result == null)
+            throw new InvalidOperationException("Failed to retrieve token");
+        return result.AccessToken;
+    }
+));
+
+try{
+var secretData = await client.GetSecretAsync(baseUri, "infrastructure-deployment-values-productionv3");
+
+Console.WriteLine($"Secret: {secretData.Value}");
+Console.ReadKey();
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+MFLOW_TEST_BASE_URL='https://your_vault.vault.azure.net/' MFLOW_TEST_CLIENT_ID=<client_test_id> MFLOW_TEST_CLIENT_SECRET='<password>' dotnet run
+
